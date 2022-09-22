43520acea5c2edeb39cb0e664af3d66886ae9db5
ALEFF JUNIO SILVA LINS LIRA <dev.alefflira@gmail.com>
2022-09-22 09:16:41 -0300

    Tipos de dados

---
+++ 2_tipos-dados.py
@@ -0,0 +1,22 @@
+int()
+float()
+bool()
+str()
+
+# Inteiros
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 5)
+
+# Float
+print(2.5 + 2.5)
+print(1 / 3)
+
+# Boolean
+print(True)
+print(False)
+
+# String
+print('Hello World!')
+print('This is a text/string')
